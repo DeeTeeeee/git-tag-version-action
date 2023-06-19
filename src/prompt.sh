1054fccb7abeca90290c0eb2ad59cd677c78a8c1
DeeTeee <ductuan7575@gmail.com>
2023-06-19 07:56:36 +0700

    Optimize code

--- src/prompt.sh
+++ src/prompt.sh
@@ -1,7 +1,8 @@
+source src/version.sh && source src/tag.sh
 get_stage_prompt() {
   title="Chon moi truong build:"
   prompt="Lua chon:"
-  options=("Development" "Staging" "Production" "Increment App Version")
+  options=("Development" "Staging" "Production" "Bump Version")
 
   echo "$title"
   PS3="$prompt "
@@ -24,22 +25,23 @@ get_stage_prompt() {
   done
 }
 
-get_version_option() {
+get_bump_version_option() {
   PREFIX='merchant/'
   if [[ "$1" ]]; then
     PREFIX="$1/"
   fi
   PRO_TAG=$(git tag --sort=-version:refname -l "${PREFIX}production/*" | head -n 1)
   title="Version của app hiện tại: $PRO_TAG"
-  options=("Major Version" "Minor Version" "Patches")
+  options=("Major Version" "Minor Version" "Patche")
+  NEW_VERSION=''
 
   echo "$title"
   echo "Lựa chọn để nâng version"
   select opt in "${options[@]}" "Thoat"; do
     case "$REPLY" in
-    1) echo "Lua chon build $opt" && bump_version "Major" $PRO_TAG && break ;;
-    2) echo "Lua chon build $opt" && bump_version "Minor" $PRO_TAG && break ;;
-    3) echo "Lua chon build $opt" && bump_version "Patches" $PRO_TAG && break ;;
+    1) echo "Lua chon build $opt" && NEW_VERSION=$(split_version $PRO_TAG increment_major) && break ;;
+    2) echo "Lua chon build $opt" && NEW_VERSION=$(split_version $PRO_TAG increment_minor) && break ;;
+    3) echo "Lua chon build $opt" && NEW_VERSION=$(split_version $PRO_TAG increment_patche) && break ;;
     $((${#options[@]} + 1)))
       echo "Goodbye!"
       exit 0
@@ -50,25 +52,5 @@ get_version_option() {
       ;;
     esac
   done
-}
-
-bump_version() {
-  NEW_VERSION=''
-  VERSION_NAME_PATH=($(grep -oE '[a-z,0-9,-.+]*' <<<"$2"))
-  ARRAY_SIZE=${#VERSION_NAME_PATH[@]}
-  VERSION=${VERSION_NAME_PATH[$((ARRAY_SIZE - 1))]}
-  ENVIRONMENT=${VERSION_NAME_PATH[$((ARRAY_SIZE - 2))]}
-  VERSION_ARRAY=($(grep -oE '[0-9]*' <<<"$VERSION"))
-  case $1 in
-  Major)
-    NEW_VERSION="${VERSION_NAME_PATH}/${ENVIRONMENT}/v$((VERSION_ARRAY[0]+1)).${VERSION_ARRAY[1]}.${VERSION_ARRAY[2]}+${VERSION_ARRAY[3]}"
-    ;;
-  Minor)
-    NEW_VERSION="${VERSION_NAME_PATH}/${ENVIRONMENT}/v${VERSION_ARRAY[0]}.$((VERSION_ARRAY[1]+1)).0+${VERSION_ARRAY[3]}"
-    ;;
-  Patches)
-    NEW_VERSION="${VERSION_NAME_PATH}/${ENVIRONMENT}/v${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.$((VERSION_ARRAY[2]+1))+${VERSION_ARRAY[3]}"
-    ;;
-  esac
   create_tag $NEW_VERSION
 }
--- src/version.sh
+++ src/version.sh
@@ -1,39 +1,52 @@
 split_version() {
   TAG_NAME=$1
-  VERSION_TYPE=$2 # full, all, major, minor, patche, increment_patche, build, increment_build
+  VERSION_TYPE=$2 # full, all, major, minor, patche, increment_patche, build, increment_build, increment_major, increment_minor, increment_patche
   VERSION_NAME_PATH=($(grep -oE '[a-z,0-9,-.+]*' <<<"$TAG_NAME"))
   ARRAY_SIZE=${#VERSION_NAME_PATH[@]}
   VERSION=${VERSION_NAME_PATH[$((ARRAY_SIZE - 1))]}
   ENVIRONMENT=${VERSION_NAME_PATH[$((ARRAY_SIZE - 2))]}
   VERSION_ARRAY=($(grep -oE '[0-9]*' <<<"$VERSION"))
+  MAJOR=${VERSION_ARRAY[0]}
+  MINOR=${VERSION_ARRAY[1]}
+  PATCHE=${VERSION_ARRAY[2]}
+  BUILD_NUMBER=${VERSION_ARRAY[3]}
   case $VERSION_TYPE in
   all)
     echo ${VERSION}
     ;;
   full)
-    echo "${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.${VERSION_ARRAY[2]}"
+    echo "$MAJOR.$MINOR.$PATCHE"
     ;;
-  increment_patche)
-    PATCHE_WILL_BUILD=$((VERSION_ARRAY[2] + 1))
-    echo "${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.${PATCHE_WILL_BUILD}"
-    ;;
+  # increment_patche)
+  #   PATCHE_WILL_BUILD=$((PATCHE + 1))
+  #   echo "$MAJOR.$MINOR.${PATCHE_WILL_BUILD}"
+  #   ;;
   major)
-    echo ${VERSION_ARRAY[0]}
+    echo $MAJOR
     ;;
   minor)
-    echo ${VERSION_ARRAY[1]}
+    echo $MINOR
     ;;
   patche)
-    echo ${VERSION_ARRAY[2]}
+    echo $PATCHE
     ;;
   build)
-    echo ${VERSION_ARRAY[3]}
+    echo $BUILD_NUMBER
     ;;
   increment_build)
-    echo ${VERSION_ARRAY[3]} + 1 | bc
+    echo $BUILD_NUMBER + 1 | bc
     ;;
   environment)
     echo ${ENVIRONMENT}
+    ;;
+  increment_major)
+    echo "${VERSION_NAME_PATH}/${ENVIRONMENT}/v$((MAJOR+1)).$MINOR.$PATCHE+$BUILD_NUMBER"
+    ;;
+  increment_minor)
+    echo "${VERSION_NAME_PATH}/${ENVIRONMENT}/v$MAJOR.$((MINOR+1)).0+$BUILD_NUMBER"
+    ;;
+  increment_patche)
+    echo "${VERSION_NAME_PATH}/${ENVIRONMENT}/v$MAJOR.$MINOR.$((PATCHE+1))+$BUILD_NUMBER"
     ;;
   esac
 }
